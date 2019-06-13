87d992c1238389d04864e6317cfb8d7f886e0353
mingyu <mingyu@qq.com>
2019-06-13 10:15:45 +0800

    把本地工程提交到git

---
+++ git-text-project/src/com/itheima/git/Firstgit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class Firstgit {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+    }
+}
